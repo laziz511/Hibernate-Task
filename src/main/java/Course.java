0f4dcc2153704da37137cd5acaf9c56b65b272ea
Laziz <lazizo2004@gmail.com>
2023-10-25 19:33:31 +0500

    Course class is added

---
+++ src/main/java/Course.java
@@ -0,0 +1,86 @@
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "\"COURSE\"")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "course_name")
+    private String courseName;
+
+    @Column(name = "course_description")
+    private String courseDescription;
+
+    @ManyToMany
+    @JoinTable(name = "course_group",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "group_id"))
+    private List<Group> groups;
+
+    public Course() {
+        // Default constructor
+    }
+    public Course(String courseName, String courseDescription, List<Group> groups) {
+        this.courseName = courseName;
+        this.courseDescription = courseDescription;
+        this.groups = groups;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
+    @Override
+    public String toString() {
+        return "Course [id=" + id + ", courseName=" + courseName + ", courseDescription=" + courseDescription + "]";
+    }
+
+    // Method to retrieve a list of students for a specific course
+    public List<Student> getStudentsForCourse(String courseName) {
+        List<Student> students = new ArrayList<>();
+        if (this.courseName.equals(courseName)) {
+            for (Group group : groups) {
+                if (group.getGroupName().equals(courseName)) {
+                    students.addAll(group.getStudents());
+                    System.out.println(" - -- - - - -  - - -- -  keldi");
+                }
+            }
+        }
+        return students;
+    }
+
+}
